340f05472d49b2085b7d415b85d591fcec3883b0
zbrose <zrbrose@gmail.com>
2022-02-05 20:57:52 -0800

    will get audio on click functionality working

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
-// get birds to change color in opposite way
-// get birds to chang color over key press and release
-// hook up key release up and down
-// add retry/button button
-// make a next level button appear when the combi is right
-// make notes appear on the lines
-// record sounds that play on key input
-// make the other keys not work
-//add click function also
+// get birds animate in some way on selection
+// get birds to change color over click press and release or mouse over, make interactive
+// record sounds and upload them into here
+// create a Bird object instance and run it for each click?
 
 let solutionMelody = ['c','e','d','g']   // randomly generate array numbers or strings for muktiple levels
 let playerChoiceMelody = []
 let tryCount = 0
-// const notationDisplay = document.getElementById('notation-display')
 const noteInput =  document.querySelector('.note-input')
 const resetButton = document.getElementById('reset-button')
 const matchDisplay = document.getElementById('instructions')
-const cNote = document.createElement('img')
-cNote.src = 'ledgerlineqnote.png'
-const dNote = document.createElement('img')
-dNote.src = 'quarternote.png'
-const eNote = document.createElement('img')
-eNote.src = 'quarternote.png'
-const gNote = document.createElement('img')
-gNote.src = 'quarternote.png'
-const aNote = document.createElement('img')
-aNote.src = 'quarternote.png'
+const  melody = document.querySelector('.melody')  
+
+// const cNote = document.createElement('img')
+// cNote.src = 'ledgerlineqnote.png'
+// const dNote = document.createElement('img')
+// dNote.src = 'quarternote.png'
+// const eNote = document.createElement('img')
+// eNote.src = 'quarternote.png'
+// const gNote = document.createElement('img')
+// gNote.src = 'quarternote.png'
+// const aNote = document.createElement('img')
+// aNote.src = 'quarternote.png'
 
 
 function lookForMatches (arr1, arr2) {                                        
@@ -34,6 +30,7 @@ function lookForMatches (arr1, arr2) {
 }
 
 const gameReset = () =>{
+        if (resetButton.innerText === 'Reset'){
         tryCount++
         document.getElementById('try-count').innerText = 'Try Count: ' + tryCount
         playerChoiceMelody = []
@@ -45,7 +42,8 @@ const gameReset = () =>{
         for (let i=0;i<nodeList.length;i++){
                nodeList[i].classList.remove('.note-c','note-d','note-e','note-g','note-a')
          }
-        console.log('reset')
+        } else console.log('next level')
+        
 }
 
 
@@ -59,41 +57,45 @@ const playerNoteChoices = document.querySelectorAll('.bird').forEach(playerNoteC
         if (playerBirdChoice === 'bird1'){
                 document.getElementById('bird1') 
                 playerChoiceMelody.push('c')
-                // const cNote = document.createElement('img')
-                // cNote.src = 'ledgerlineqnote.png'
+                const cNote = document.createElement('img')
+                cNote.src = 'ledgerlineqnote.png'
                 cNote.classList.add('note-c','note')
                 noteInput.appendChild(cNote)
+                melody.play()
         }  if (playerBirdChoice === 'bird2'){
                 document.getElementById('bird2') 
                 playerChoiceMelody.push('d')
-                // const dNote = document.createElement('img')
-                // dNote.src = 'quarternote.png'
+                const dNote = document.createElement('img')
+                dNote.src = 'quarternote.png'
                 dNote.classList.add('note-d','note')
                 noteInput.appendChild(dNote)
         } if (playerBirdChoice === 'bird3'){
                 document.getElementById('bird3') 
                 playerChoiceMelody.push('e')
-                // const eNote = document.createElement('img')
-                // eNote.src = 'quarternote.png'
+                const eNote = document.createElement('img')
+                eNote.src = 'quarternote.png'
                 eNote.classList.add('note-e','note')
                 noteInput.appendChild(eNote)
         } if (playerBirdChoice === 'bird4'){
                 document.getElementById('bird4') 
                 playerChoiceMelody.push('g')
-                // const gNote = document.createElement('img')
-                // gNote.src = 'quarternote.png'
+                const gNote = document.createElement('img')
+                gNote.src = 'quarternote.png'
                 gNote.classList.add('note-g','note')
                 noteInput.appendChild(gNote)
         } if (playerBirdChoice === 'bird5'){
                 document.getElementById('bird5') 
                 playerChoiceMelody.push('a')
-                // const aNote = document.createElement('img')
-                // aNote.src = 'quarternote.png'
+                const aNote = document.createElement('img')
+                aNote.src = 'quarternote.png'
                 aNote.classList.add('note-a','note')
                 noteInput.appendChild(aNote)
         } 
         if (lookForMatches(playerChoiceMelody, solutionMelody)===true){
                 matchDisplay.innerText = 'WOOP! YOUR EARS ARE LOOKING BIGGER ALREADY!'
+                resetButton.innerText = "Next Level"
+                //stop player from inputting notes
+                removeEventListener('click', playerNoteChoices)
                 console.log('WE HAVE A WINNER')
         
         } else if (playerChoiceMelody.length === solutionMelody.length && lookForMatches(playerChoiceMelody, solutionMelody)===false) {
